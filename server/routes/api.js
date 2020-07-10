7eb20ade3ff4576121d471891d8712eed11daab1
pamuba <pamuba@users.noreply.github.com>
2020-07-10 22:26:12 +0530

    1st

---
+++ server/routes/api.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const router = express.Router()
+const User = require('../models/user')
+
+const mongoose = require('mongoose')
+
+const db = "mongodb://localhost:27017/authdb"
+
+mongoose.connect(db, err => {
+    if(err){
+        console.error('Error!' + err)
+    }else{
+        console.log('Connected to mongodb')
+    }
+})
+
+router.get('/', (req, res)=>{
+    res.send('From API route')
+})
+
+router.post('/register', (req,res) => {
+    let userData = req.body
+    let user = new User(userData)
+    user.save((error, registeredUser)=>{
+        if(error){
+            console.log(error)
+        }
+        else{
+            res.status(200).send(registeredUser)
+        }
+    })
+})
+
+module.exports = router
